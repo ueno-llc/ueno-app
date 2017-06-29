7a971b2aceeb3770eb0e57323b67d54bee784e53
Birkir Gudjonsson <birkir.gudjonsson@gmail.com>
2017-06-29 09:47:41 -0400

    Upgrade of podfile and migrate

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -4,17 +4,20 @@ if [ $DIR == "scripts" ]; then
   exit 1
 fi
 
+NODE_MODULES="${PWD}/node_modules"
+NATIVE_NAVIGATION="$NODE_MODULES/native-navigation/lib/android"
+
 # ANDROID
-FILE1=$(realpath ./node_modules/native-navigation/lib/android/src/main/res/layout/fragment_react_native.xml)
-FILE2=$(realpath ./node_modules/native-navigation/lib/android/src/main/java/com/airbnb/android/react/navigation/ReactNativeFragment.java)
+FILE1="$NATIVE_NAVIGATION/src/main/res/layout/fragment_react_native.xml"
+FILE2="$NATIVE_NAVIGATION/src/main/java/com/airbnb/android/react/navigation/ReactNativeFragment.java"
 patch -sN $FILE1 < ./scripts/fragment_react_native.xml.patch
 patch -sN $FILE2 < ./scripts/ReactNativeFragment.java.patch
-sed -i'.bak' -e 's/0.11.0/1.0.1/' ./node_modules/native-navigation/lib/android/build.gradle
-rm -rf ./node_modules/native-navigation/lib/android/src/main/res/layout/fragment_react_native.xml.rej
-rm -rf ./node_modules/native-navigation/lib/android/src/main/java/com/airbnb/android/react/navigation/ReactNativeFragment.java.rej
+sed -i'.bak' -e 's/0.11.0/1.0.1/' "$NODE_MODULES/native-navigation/lib/android/build.gradle"
+rm -rf "$FILE1.rej"
+rm -rf "$FILE2.rej"
 
 # IOS
-sed -i'.bak' -e 's/#import <RCTAnimation\\/RCTValueAnimatedNode.h>/#import "RCTValueAnimatedNode.h"/' ./node_modules/react-native/Libraries/NativeAnimation/RCTNativeAnimatedNodesManager.h
+sed -i'.bak' -e 's/#import <RCTAnimation\\/RCTValueAnimatedNode.h>/#import "RCTValueAnimatedNode.h"/' "$NODE_MODULES/react-native/Libraries/NativeAnimation/RCTNativeAnimatedNodesManager.h"
 
 # DONE
-echo "===> Done patching files in node_modules"
+echo "===> Done patching files in $NODE_MODULES"
