d5a3c1a75117504e5d54aea78b6962eb991bdef8
Birkir Gudjonsson <birkir.gudjonsson@gmail.com>
2017-06-29 10:48:32 -0400

    Using patch for everything

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -9,15 +9,21 @@ NATIVE_NAVIGATION="$NODE_MODULES/native-navigation/lib/android"
 
 # ANDROID
 FILE1="$NATIVE_NAVIGATION/src/main/res/layout/fragment_react_native.xml"
-FILE2="$NATIVE_NAVIGATION/src/main/java/com/airbnb/android/react/navigation/ReactNativeFragment.java"
 patch -sN $FILE1 < ./scripts/fragment_react_native.xml.patch
-patch -sN $FILE2 < ./scripts/ReactNativeFragment.java.patch
-sed -i'.bak' -e 's/0.11.0/1.0.1/' "$NODE_MODULES/native-navigation/lib/android/build.gradle"
 rm -rf "$FILE1.rej"
+
+# ReactNativeFragment.java
+FILE2="$NATIVE_NAVIGATION/src/main/java/com/airbnb/android/react/navigation/ReactNativeFragment.java"
+patch -sN $FILE2 < ./scripts/ReactNativeFragment.java.patch
 rm -rf "$FILE2.rej"
 
+# Build.gradle
+sed -i'.bak' -e 's/0.11.0/1.0.1/' "$NODE_MODULES/native-navigation/lib/android/build.gradle"
+
 # IOS
-sed -i'.bak' -e 's/#import <RCTAnimation\\/RCTValueAnimatedNode.h>/#import "RCTValueAnimatedNode.h"/' "$NODE_MODULES/react-native/Libraries/NativeAnimation/RCTNativeAnimatedNodesManager.h"
+FILE3="$NODE_MODULES/react-native/Libraries/NativeAnimation/RCTNativeAnimatedNodesManager.h"
+patch -sN $FILE3 < ./scripts/RCTNativeAnimatedNodesManager.h.patch
+rm -rf "$FILE3.rej"
 
 # DONE
 echo "===> Done patching files in $NODE_MODULES"
